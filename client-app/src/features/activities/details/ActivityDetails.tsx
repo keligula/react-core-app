a9feae3cf6c8250f1f1408413256c637a6a9c39b
Kelly Smith <17773027+keligula@users.noreply.github.com>
2024-07-19 15:13:13 -0700

    End of section 7

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,13 +1,13 @@
 import { Button, Card, CardContent, CardDescription, CardHeader, CardMeta, Image } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
+import { useStore } from "../../../app/stores/store";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
-interface Props {
-    activity: Activity;
-    cancelSelectActivity: () => void;
-    openForm: (id: string) => void;
-}
+export default function ActivityDetails() {
+    const {activityStore} = useStore();
+    const {selectedActivity: activity, openForm, cancelSelectActivity} = activityStore;
 
-export default function ActivityDetails({ activity, cancelSelectActivity, openForm }: Props) {
+    if (!activity) return <LoadingComponent content={""} />;
+
     return (
         <Card>
             <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
