d1bbe3189713d41ef7371fe7d8d54818260debea
Kelly Smith <17773027+keligula@users.noreply.github.com>
2024-07-22 15:55:17 -0700

    End of section 10

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -11,7 +12,9 @@ builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline. Middleware section.
+// Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Container, Menu } from 'semantic-ui-react';
+import { Button, Container, Menu } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
@@ -10,7 +10,10 @@ export default function NavBar() {
                     React-Core-App
                 </Menu.Item>
                 <Menu.Item as={NavLink} to='/activities' name='Activities List' />
-                <Menu.Item as={NavLink} to='/createActivity' content='Create Activity' />
+                <Menu.Item as={NavLink} to='/errors' name='Errors' />
+                <Menu.Item>
+                    <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
+                </Menu.Item>
             </Container>
         </Menu>
     )
