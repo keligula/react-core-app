5fff0457170041bbf697e57a45c802fcb9a7fac2
Kelly Smith <17773027+keligula@users.noreply.github.com>
2024-08-14 17:27:48 -0700

    End of section 16

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,6 +12,7 @@ namespace Persistence
 
         public DbSet<Activity> Activities { get; set; }
         public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+        public DbSet<Photo> Photos { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -23,7 +24,7 @@ namespace Persistence
                 .HasOne(u => u.AppUser)
                 .WithMany(a => a.Activities)
                 .HasForeignKey(aa => aa.AppUserId);
-            
+
             builder.Entity<ActivityAttendee>()
                 .HasOne(u => u.Activity)
                 .WithMany(a => a.Attendees)
